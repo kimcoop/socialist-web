7236263690b74fa10e456e3340f2885174c43726
Kim Cooperrider <kim.cooperrider@gmail.com>
2012-10-16 17:04:16 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialist::Application.config.secret_token = 'fd9f48044b900e781a8ac96055cdcb9251c997807ec25ee5fd85a0400139a504ba6dfa6ce099823d7f25796b55a02db3c04b05f2ad46e9554bb9158dff515865'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Socialist::Application.config.session_store :cookie_store, key: '_socialist_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Socialist::Application.config.session_store :active_record_store
